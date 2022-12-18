df963869b6fff47159b3b8c70ec6ebfbcb366786
Hoang Nguyen <royal@Hoangs-MacBook-Pro-3.local>
2022-12-18 14:06:40 +0700

    [royal nguyen] Update

--- app/server/app.js
+++ app/server/app.js
@@ -20,10 +20,6 @@ routes.users(app);
 connectDB()
   .then(() => {
     console.log("Connected to the database!");
-    const PORT = process.env.NODE_DOCKER_PORT || 8080;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}.`);
-    });
   })
   .catch((err) => {
     console.log("Cannot connect to the database!", err);
