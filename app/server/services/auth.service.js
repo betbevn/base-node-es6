4e1b30163328e710c7da1c6fdb39ead2e14391df
Hoang Nguyen <royal@Hoangs-MacBook-Pro-3.local>
2023-01-11 14:22:21 +0700

    [royal nguyen] Update

--- app/server/services/auth.service.js
+++ app/server/services/auth.service.js
@@ -12,7 +12,7 @@ const login = async (req, res) => {
   } else if (user.validatePassword(req.body.password)) {
     const token = generateToken(user);
     res.cookie("access_token", token, {
-      maxAge: 5 * 60 * 1000, // milliseconds
+      maxAge: new Date(Date.now() + 5 * 60 * 1000), // milliseconds
       httpOnly: true,
       // secure: true;
     });
@@ -42,7 +42,7 @@ const signup = async (req, res) => {
   const user = await newUser.save();
   const token = generateToken(user);
   res.cookie("access_token", token, {
-    maxAge: 5 * 60 * 1000, // milliseconds
+    maxAge: new Date(Date.now() + 5 * 60 * 1000), // milliseconds
     httpOnly: true,
     // secure: true;
   });
