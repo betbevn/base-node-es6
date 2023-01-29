e9227dc84b42fbf38b285aa7054c81d76b0ee34b
Hoang Nguyen <royal@Hoangs-MacBook-Pro-3.local>
2023-01-29 20:12:50 +0700

    [royal nguyen] Update

--- baby-service/gulpfile.js
+++ baby-service/gulpfile.js
@@ -1,19 +1,25 @@
 // Import `src` and `dest` from gulp for use in the task.
-const { src, dest } = require("gulp");
+const gulp = require("gulp");
 
 // Import Gulp plugins.
 const babel = require("gulp-babel");
 const plumber = require("gulp-plumber");
 
-exports.default = function () {
+function bundleJs() {
   return (
-    // src("./src/components/**/*.js")
-    src("./server/**/*.js")
+    gulp
+      .src("./server/**/*.js")
       // Stop the process if an error is thrown.
       .pipe(plumber())
       // Transpile the JS code using Babel's preset-env.
       .pipe(babel())
       // Save each component as a separate file in dist.
-      .pipe(dest("./build"))
+      .pipe(gulp.dest("./build"))
   );
-};
+}
+
+function bundlePem() {
+  return gulp.src("./server/**/*.pem").pipe(gulp.dest("./build"));
+}
+
+exports.default = gulp.series(bundleJs, bundlePem);
